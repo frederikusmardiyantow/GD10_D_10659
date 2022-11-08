01f12a407d4f2557ba40c62b7110eb7c62c95f41
Frederikus Mardiyanto W <94357450+frederikusmardiyantow@users.noreply.github.com>
2022-11-08 09:49:42 +0700

    UGD 10 Android

---
+++ app/src/main/java/com/example/gd10_d_10659/ResponseDataMahasiswa.kt
@@ -0,0 +1,9 @@
+package com.example.gd10_d_10659
+
+import com.google.gson.annotations.SerializedName
+
+data class ResponseDataMahasiswa(
+    @SerializedName("status") val stt:String,
+    val totaldata: Int,
+    val data:List<MahasiswaData>
+    )
